128e309eaebff7111864af2b262914e141d28cde
BOT <42821200+knownasbot@users.noreply.github.com>
2022-08-24 16:43:29 -0300

    Made it faster and better

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { blueBright, redBright, gray, greenBright, yellow } = require("chalk");
 // CLI config
 const params = cli.parse({
     ip: [ false, "IP to start the scanning", "ip" ],
+    num: [ "n", "The number of servers that will be scanned per iteration. Higher levels may cause slowdowns (default: 10)", "int" ],
     port: [ "p", "Port to check (default: 25565)", "int" ],
     output: [ "o", "Output file path", "path" ],
     timeout: [ "t", "Timeout for server response in milliseconds (default: 5000)", "int" ]
@@ -23,6 +24,7 @@ if (!params.ip) {
 const timeout = params.timeout ?? 5000;
 const currentIP = params.ip.split(".").map(v => parseInt(v));
 const port = params.port ?? 25565;
+const num = params.num ?? 10;
 
 let output = "";
 function log(a) {
@@ -71,58 +73,59 @@ function queryIP(ip) {
 }
 
 function checkIPs() {
-    for (let i = 0; i < 10; i++) {
-        queryIP(currentIP.join("."))
-        .then((ip) => {
-            if (ip) {
-                status(ip, port)
-                .then((status) => {
-                    let playersInfo = "Players: " + gray(`${status.players.online}/${status.players.max}`);
-                    let motd = "MOTD: " + gray(status.motd.clean.includes("\n") ? "\n" + status.motd.clean : status.motd.clean);
+    queryIP(currentIP.join("."))
+    .then((ip) => {
+        if (ip) {
+            status(ip, port)
+            .then((status) => {
+                let playersInfo = "Players: " + gray(`${status.players.online}/${status.players.max}`);
+                let motd = "MOTD: " + gray(status.motd.clean.includes("\n") ? "\n" + status.motd.clean : status.motd.clean);
 
-                    if (status.players.sample?.length > 0) {
-                        const playersUsernames = status.players.sample.filter((v) => v.name.length > 3 && v.name.length < 17).map((v) => v.name).slice(0, 10);
-                        if (playersUsernames.length > 0) {
-                            if (status.players.sample.length > 10) {
-                                playersUsernames.push("...");
-                            }
-
-                            playersInfo += ` (${gray(playersUsernames.join(blueBright(", ")))})`;
+                if (status.players.sample?.length > 0) {
+                    const playersUsernames = status.players.sample.filter((v) => v.name.length > 3 && v.name.length < 17).map((v) => v.name).slice(0, 10);
+                    if (playersUsernames.length > 0) {
+                        if (status.players.sample.length > 10) {
+                            playersUsernames.push("...");
                         }
+
+                        playersInfo += ` (${gray(playersUsernames.join(blueBright(", ")))})`;
                     }
+                }
 
-                    log(blueBright(`${greenBright("[+]")} ${gray(`${ip}:${port}`)}\n${playersInfo}\n${motd}\n`));
-                })
-                .catch((e) => {
-                    log(blueBright(`${greenBright("[+]")} ${gray(`${ip}:${port}`)}\nFailed to get MOTD info:\n${gray(e)}\n`));
-                });
-            }
-        });
+                log(blueBright(`${greenBright("[+]")} ${gray(`${ip}:${port}`)}\n${playersInfo}\n${motd}\n`));
+            })
+            .catch((e) => {
+                log(blueBright(`${greenBright("[+]")} ${gray(`${ip}:${port}`)}\nFailed to get MOTD info:\n${gray(e)}\n`));
+            });
+        }
 
-        currentIP[3]++;
-        if (currentIP[3] > 255) {
-            currentIP[3] = 0;
+        checkIPs();
+    });
 
-            currentIP[2]++;
-            if (currentIP[2] > 255) {
-                currentIP[2] = 0;
-    
-                currentIP[1]++;
-                if (currentIP[1]++ > 255) {
-                    currentIP[1] = 0;
+    currentIP[3]++;
+    if (currentIP[3] > 255) {
+        currentIP[3] = 0;
 
-                    currentIP[0]++;
-                    if (currentIP[0]) {
-                        log(redBright("[x] Reached the IP limit."));
-                        process.exit(1);
-                    }
+        currentIP[2]++;
+        if (currentIP[2] > 255) {
+            currentIP[2] = 0;
+
+            currentIP[1]++;
+            if (currentIP[1]++ > 255) {
+                currentIP[1] = 0;
+
+                currentIP[0]++;
+                if (currentIP[0]) {
+                    log(redBright("[x] Reached the IP limit."));
+                    process.exit(1);
                 }
             }
         }
     }
 }
 
-setInterval(() => checkIPs(), timeout);
-checkIPs();
+for(let i = 0; i < num; i++) {
+    checkIPs();
+}
 
 console.log(yellow("[!] Scanning...\n"));
